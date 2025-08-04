07022ae1c64e0a5b04bb720e38d8f6bf05e514ec
Cristhian de León <deleonperezcristhianestuardo@gmail.com>
2025-08-04 01:39:19 -0600

    Se creearon las funciones para la segunda y tercera opción, también se hicieron algunas correcciones en en la primera opción para solo trabajar con una lista principal y una verdadera sublista para los cursos

--- ejercicio_13.py
+++ ejercicio_13.py
@@ -1,10 +1,39 @@
 students = {}
-notas = {}
 def add_student():
-    id_student = str(input("Ingrese el ID del eestudiante: "))
+    id_student = str(input("Ingrese el ID del estudiante: "))
     name_student = str(input("Ingrese el nombre del estudiante: "))
-    course_student = str(input("Ingrese el curso o programa academico: "))
-    students[id_student] = {"nombre":name_student, "curso":course_student}
+    degree_student = str(input("Ingrese la carrera o programa academico: "))
+    students[id_student] = {"nombre":name_student, "carrera":degree_student, "cursos":{}}
+
+def add_course():
+    if students:
+        id_search2 = str(input("Ingrese el ID del estudiante: "))
+        if id_search2 in students:
+            course_name = str(input("Ingrese el nombre del curso: "))
+            final_qualification = float(input("Ingrese la nota final del estudiante: "))
+            students[id_search2]['cursos'][course_name] = final_qualification
+        else:
+            print(f"El ID {id_search2} no se ha encontrado en la base de datos")
+    else:
+        print("No hay ningun estudiante añadido")
+
+def consult_student():
+    if students:
+        id_search = str(input("Ingrese el ID del estudiante a consultar: "))
+        if id_search in students:
+            print(f"Nombre del estudiante: {students[id_search]['nombre']}")
+            print(f"Carrera o programa academico: {students[id_search]['carrera']}")
+            if students[id_search]['cursos']:
+                print("Cursos añadidos")
+                for course, qualification in students[id_search]["cursos"].items():
+                    print(f"Curso: {course}, Nota Final: {qualification}")
+            else:
+                print("No se ha añadido algún curso al estudiante")
+        else:
+            print(f"El ID {id_search} no se ha encontrado en la base de datos")
+    else:
+        print("No hay ningun estudiante añadido")
+
 
 while True:
     print("--Menú--")
@@ -21,10 +50,15 @@ while True:
         case "1":
             print("Agregar Estudiante")
             add_student()
+            print()
         case "2":
             print("Agregar Curso con Nota")
+            add_course()
+            print()
         case "3":
             print("Consultar Estudiante")
+            consult_student()
+            print()
         case "4":
             print("Calcular promedio del estudiante")
         case "5":
